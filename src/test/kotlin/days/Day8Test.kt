79140a767b65a54bdb1f2eef108c0aeb2e3b9e8a
Andrei Purcaru <andrei.purcaru@teampicnic.com>
2023-12-09 19:15:11 +0100

    Implement day 8

---
+++ src/test/kotlin/days/Day8Test.kt
@@ -0,0 +1,14 @@
+package days
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Day8Test {
+
+    @Test
+    fun solve() {
+        val day8 = Day8()
+        println(day8.solvePart2())
+    }
+}
